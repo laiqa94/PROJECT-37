90a87f65eedf032fade004572bce57058e3b11e3
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 03:00:33 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+
+function make_shirt(size:string= 'Large', text:string ='I love TypeScript.'){
+
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+
+make_shirt();
+
+make_shirt('Mudium');
+
+make_shirt('Small','I love python');
